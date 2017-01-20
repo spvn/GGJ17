e1426444c24c39387e116eb56ad3508f83573620
pigoblock <kowanling@hotmail.com>
2017-01-20 21:45:24 +0800

    Player movement in progress

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float sidewayMovementSpeed = 5f;
+	public float maxSidewayMovement = 2.82f;
+
+	private Vector3 startPosition;
+	private Vector3 boost;
+
+	void Start () {
+		startPosition = transform.position;
+	}
+
+	void Update () {
+		if (Input.GetKey (KeyCode.Space)) {
+			Debug.Log ("holding spacebar");
+		}
+
+		transform.position = startPosition + new Vector3 (
+			maxSidewayMovement * Mathf.Sin (Time.time * sidewayMovementSpeed) ,
+			startPosition.y,
+			startPosition.z
+		);
+	}
+}
+
