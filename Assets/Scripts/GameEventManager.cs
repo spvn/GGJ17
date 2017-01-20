ed706a8c872e5715f8887d400fecf227ff2be054
pigoblock <kowanling@hotmail.com>
2017-01-20 20:42:07 +0800

    Gameeventmanager and health script

---
+++ Assets/Scripts/GameEventManager.cs
@@ -0,0 +1,26 @@
+﻿public static class GameEventManager {
+
+	public delegate void GameEvent();
+
+	public static event GameEvent GameStart;
+	public static event GameEvent GameOver;
+	public static event GameEvent TitleScreen;
+
+	public static void TriggerGameStart() {
+		if (GameStart != null) {
+			GameStart ();
+		}
+	}
+
+	public static void TriggerGameOver() {
+		if (GameOver != null) {
+			GameOver ();
+		}
+	}
+
+	public static void TriggerTitleScreen() {
+		if (TitleScreen != null) {
+			TitleScreen ();
+		}
+	}
+}
---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+
+	public float maxHealth;
+	public bool isDead = false;
+
+	protected float currentHealth;
+	private bool hasTriggerGameOver = false;
+
+	void Awake () {
+		GameEventManager.GameStart += ResetHealth ();
+	}
+
+	void Start() {
+		ResetHealth ();
+	}
+
+	void Update () {
+		if (isDead && !hasTriggerGameOver) {
+			hasTriggerGameOver = true;
+			GameEventManager.TriggerGameOver ();
+		}
+	}
+
+	private void ResetHealth() {
+		currentHealth = maxHealth;
+		hasTriggerGameOver = false;
+	}
+
+	public virtual void reduceHealth(float amt){
+		currentHealth -= amt;
+
+		if (currentHealth < 0f) {
+			currentHealth = 0f;
+			isDead = true;
+		}
+	}
+
+	public virtual void addHealth(float amt){
+		currentHealth += amt;
+
+		if (currentHealth > maxHealth) {
+			currentHealth = maxHealth;
+		}
+	}
+
+	public float getHealthProportion() {
+		return currentHealth / maxHealth;
+	}
+
+	public float getCurrentHealth() {
+		return currentHealth;
+	}
+}
