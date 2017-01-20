1f9419cb2f9a915b0ee26e7de41770ed8e9fe04c
spvn <wkan2003@gmail.com>
2017-01-20 21:48:29 +0800

    Spawner WIP

---
+++ Assets/Scripts/AsteroidSpawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidSpawner : MonoBehaviour {
+
+	public float spawnInterval = 3f;
+	public GameObject asteroid;
+
+	private float currSpawnInterval = 0f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		currSpawnInterval += Time.deltaTime;
+
+		if (currSpawnInterval >= spawnInterval) {
+			spawnAsteroid ();
+			currSpawnInterval = 0f;
+		}
+	}
+
+	void spawnAsteroid() {
+		GameObject.Instantiate (asteroid,transform.position, Quaternion.identity);
+	}
+}
