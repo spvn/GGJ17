501b45691bbdecc91aef6a0d368315a09f00b0e4
spvn <wkan2003@gmail.com>
2017-01-21 18:02:38 +0800

    Added drunk backgrounds

--- Assets/Scripts/BackgroundScroll.cs
+++ Assets/Scripts/BackgroundScroll.cs
@@ -4,12 +4,17 @@ using UnityEngine;
 
 public class BackgroundScroll : MonoBehaviour {
 
+	public bool drunk = false;
 	public float speed = 0.5f;
+	public float colourSpeed = 1f;
+	private bool isRandomising = false;
+	private IEnumerator coroutine;
 
 	private MeshRenderer renderer;
 	// Use this for initialization
 	void Start () {
 		renderer = GetComponent<MeshRenderer> ();
+		coroutine = randomiseColours ();
 	}
 	
 	// Update is called once per frame
@@ -17,5 +22,48 @@ public class BackgroundScroll : MonoBehaviour {
 		Vector2 offset = new Vector2 (0f, Time.time * speed);
 
 		renderer.material.mainTextureOffset = offset;
+
+		if (drunk && !isRandomising) {
+			
+			StartCoroutine (coroutine);
+			isRandomising = true;
+			//renderer.material.SetColor ("_Color", new Color32 (255, 0, 0, 255));
+		} else {
+			isRandomising = false;
+			StopCoroutine (coroutine);
+			renderer.material.SetColor ("_Color", Color.white);
+		}
+
+	}
+	/*
+	IEnumerator randomiseColours() {
+
+		while (true) {
+			float randHue = Random.Range (0f, 1f);
+
+			Color32 randColor = Color.HSVToRGB (randHue, 1f, 1f);
+
+			Color32 currColor = renderer.material.color;
+			float duration = 0f;
+
+			while (duration < 0.5f) {
+				renderer.material.SetColor ("_Color", Color.Lerp (currColor, randColor, duration / 0.5f));
+				duration += Time.deltaTime;
+				yield return null;
+			}
+
+			yield return null;
+		}
+
+	}*/
+
+	IEnumerator randomiseColours() {
+		while (true) {
+			float hue = Mathf.PingPong (Time.time * colourSpeed, 1f);
+			Debug.Log (hue);
+
+			renderer.material.SetColor ("_Color", Color.HSVToRGB (hue, 1f, 1f));
+			yield return null;
+		}
 	}
 }
