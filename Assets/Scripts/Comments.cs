65a65ae9900a2688d17b082ca087a3a7e12373f3
pigoblock <kowanling@hotmail.com>
2017-01-21 16:42:03 +0800

    Update player prefab

---
+++ Assets/Scripts/Comments.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Comments : MonoBehaviour {
+
+	public List<string> comments;
+
+	private TextMesh tm;
+
+	void Start () {
+		tm = GetComponent<TextMesh> ();
+	}
+	
+
+	void Update () {
+		
+	}
+}
