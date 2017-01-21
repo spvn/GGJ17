777791a60d2b6262b8ef5f1648c6fbcfa627e15d
spvn <wkan2003@gmail.com>
2017-01-21 19:28:49 +0800

    Vomit Spawner

---
+++ Assets/Scripts/VomitSpawner.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VomitSpawner : MonoBehaviour {
+
+	public Sprite[] sprites;
+
+	private SpriteRenderer[] renderers;
+	// Use this for initialization
+	void Start () {
+		renderers = GetComponentsInChildren<SpriteRenderer> ();
+		SpawnVomit ();
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void SpawnVomit() {
+		
+
+		foreach (SpriteRenderer sr in renderers) {
+			sr.sprite = sprites [Random.Range (0, sprites.Length)];
+			sr.color = new Color32 (73, 216, 109, 255);
+		}
+
+		StartCoroutine (fadeOut());
+
+	}
+
+	IEnumerator fadeOut() {
+
+		yield return new WaitForSeconds (2f);
+
+		float duration = 0f;
+		float totalTime = 1f;
+
+		while (duration < totalTime) {
+			foreach (SpriteRenderer sr in renderers) {
+				
+				sr.color = Color32.Lerp (new Color32 (73, 216, 109, 255), new Color32 (73, 216, 109, 0), duration / totalTime);
+			}
+			duration += Time.deltaTime;
+			yield return null;
+		}
+	}
+}
