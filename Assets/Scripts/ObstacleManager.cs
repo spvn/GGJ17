4f28bd6769cd83bf80275a7bf045bc10acfd0766
spvn <wkan2003@gmail.com>
2017-01-21 12:25:22 +0800

    Randomised enemy spawning and added waves Added ObstacleManager

---
+++ Assets/Scripts/ObstacleManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleManager : MonoBehaviour {
+
+	public static int enemyCount = 0;
+	public static int maxEnemyCount = 1;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
