ff8dce0ceec79c562a1227a1af55fc20c77eebe0
pigoblock <kowanling@hotmail.com>
2017-01-21 11:29:40 +0800

    Fix player movement

---
+++ Assets/Scripts/Thruster.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Thruster : MonoBehaviour {
+
+	public float maxThrustSize = 2f;
+
+	private PlayerMovement pmovement;
+	private Vector3 originalLocalScale;
+
+	// Use this for initialization
+	void Start () {
+		pmovement = GetComponentInParent<PlayerMovement> ();
+	
+		originalLocalScale = transform.localScale;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float thrusterBoost = pmovement.getHoldRatio();
+	
+		transform.localScale = originalLocalScale 
+			+ originalLocalScale * thrusterBoost * maxThrustSize;
+	}
+}
