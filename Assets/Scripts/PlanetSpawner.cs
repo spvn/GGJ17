077fba8f34594be75880b9a6f81cc1d769558885
spvn <wkan2003@gmail.com>
2017-01-21 21:15:35 +0800

    Adding sounds WIP

--- Assets/Scripts/PlanetSpawner.cs
+++ Assets/Scripts/PlanetSpawner.cs
@@ -18,7 +18,7 @@ public class PlanetSpawner : MonoBehaviour {
 
 	void SpawnPlanets() {
 		if (Random.Range (0f, 1f) < 0.3f) {
-			int index = Random.Range (0, planets.Length - 1);
+			int index = Random.Range (0, planets.Length);
 			float xPos = Random.Range (-3.5f, 3.5f);
 
 			Instantiate (planets [index], new Vector3 (xPos, transform.position.y, 0f), Quaternion.identity);
